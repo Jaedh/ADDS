84d43beee3191a4accf788d6231f04639ab428f6
Jaedh <jaedh.rameezdeen.jr@gmail.com>
2023-03-07 19:56:31 +1030

    Prac2 attempt 1

--- Solver.cpp
+++ Solver.cpp
@@ -5,22 +5,22 @@
 Solver::Solver(Move* _player1, Move* _player2){
     player1 = _player1;
     player2 = _player2;
-    win["Rock"] = {"Scissor"};
-    win["Paper"] = {"Rock"};
-    win["Scissor"] = {"Paper"};
-    win["Monkey"] = {"Ninja", "Robot"};
-    win["Robot"] = {"Ninja", "Zombie"};
-    win["Pirate"] = {"Robot", "Monkey"};
-    win["Ninja"] = {"Pirate", "Zombie"};
-    win["Zombie"] = {"Pirate", "Monkey"};
+    win["Rock"] = {"Paper"};
+    win["Paper"] = {"Scissor"};
+    win["Scissor"] = {"Rock"};
+    win["Monkey"] = {"Zombie", "Pirate"};
+    win["Robot"] = {"Pirate", "Monkey"};
+    win["Pirate"] = {"Zombie", "Ninja"};
+    win["Ninja"] = {"Robot", "Monkey"};
+    win["Zombie"] = {"Robot", "Ninja"};
 }
 
 bool Solver::Solve(){
     std::vector<std::string> temp = win[player1->getName()];
     if(std::find(temp.begin(), temp.end(), player2->getName()) != temp.end()){
-        return true;
+        return false;
     }
-    return false;
+    return true;
 }
 
 Solver::~Solver(){
