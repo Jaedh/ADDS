06bd4b4ce24216046066f158457f92558ca8aa22
Jaedh <jaedh.rameezdeen.jr@gmail.com>
2023-03-31 02:28:47 +1030

    Initial

--- Finder.cpp
+++ Finder.cpp
@@ -7,13 +7,13 @@
         vector<int> result;
         size_t temp;
 
-        result.insert(result.begin(),s2.size(),-1);
+        // result.insert(result.begin(),s2.size(),-1);
 
         for(size_t i = 1; i <= s2.size(); i++){
             if(temp != -1){
                 temp = s1.find(s2.substr(0,i));
             }               
-            result[i-1] = temp;
+            result.push_back(temp);
         }
         return result;
     }
