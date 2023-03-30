2b94a13c09d91776689cf3286903e4913f7ee291
Jaedh <jaedh.rameezdeen.jr@gmail.com>
2023-03-31 02:07:25 +1030

    Initial

--- Finder.cpp
+++ Finder.cpp
@@ -5,20 +5,18 @@
     vector<int> Finder::findSubstrings(string s1, string s2) {
 
         vector<int> result;
-        vector<string> prefixes;
         size_t temp;
 
         for(size_t i = 1; i <= s2.size(); i++){
             result.push_back(-1);
-            prefixes.push_back(s2.substr(0,i));
         }
 
-        for(size_t i = 0; i <= prefixes.size(); i++){
-            temp = s1.find(prefixes[i]);
-            if (temp == string::npos) {
+        for(size_t i = 1; i <= s2.size(); i++){
+            temp = s1.find(s2.substr(0,i));
+            if (temp == -1) {
                 return result;
             }                  
-            result[i] = temp;
+            result[i-1] = temp;
         }
         return result;
     }
