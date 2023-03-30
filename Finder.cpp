086460d6463d21b2aac9ecb813ea378b06428996
Jaedh <jaedh.rameezdeen.jr@gmail.com>
2023-03-31 03:18:44 +1030

    Initial

--- Finder.cpp
+++ Finder.cpp
@@ -3,22 +3,15 @@
     using namespace std;
 
     vector<int> Finder::findSubstrings(string s1, string s2) {
-
-        vector<int> result;
-        size_t temp;
-
-        // result.insert(result.begin(),s2.size(),-1);
-
-        for(size_t i = 1; i <= s2.size(); i++){
-            if(temp != -1){
-                if(i>1 && i < s2.size()){
-                    s1.erase(s1.begin()+result[i-1], s1.begin()+result[i]);
-                }                
-                temp = s1.find(s2.substr(0,i));
-            }               
-            result.push_back(temp);
+    vector<int> result;
+    result.reserve(s2.size());
+    for(size_t i = 1; i <= s2.size(); i++) {
+        size_t temp = s1.find(s2.substr(0, i));
+        if (temp != string::npos) {
+            result.push_back(static_cast<int>(temp));
         }
-        return result;
+    }
+    return result;
     }
 
    
