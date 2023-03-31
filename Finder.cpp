c7d1ceaf06322c1951d5b38c6eb05bd3a6e870b8
Jaedh <jaedh.rameezdeen.jr@gmail.com>
2023-03-31 23:51:15 +1030

    Maps

--- Finder.cpp
+++ Finder.cpp
@@ -3,30 +3,19 @@
     using namespace std;
 
     vector<int> Finder::findSubstrings(string s1, string s2) {
-        vector<int> result;
-        map<string, size_t> permuations;
+           vector<int> result;
+        size_t temp;
 
-        for(size_t i=s1.size(); i >= 1; i--){
-            for(size_t j=s1.size()-i; j >= 1; j--){
-                // std::cout<<s1.substr(i,j)<<std::endl;
-                permuations[s1.substr(i-1,j)] = (size_t)(i-1);
-            }
-            // std::cout<<std::endl;
+        for(size_t i = 1; i <= s2.size(); i++){
+            if(temp != -1){             
+                temp = s1.find(s2.substr(0,i));
+            }               
+            result.push_back(temp);
         }
-
-
-
-
-
-        for(size_t i=1; i<=s2.size(); i++){
-            if(permuations.find(s2.substr(0,i))==permuations.end()){
-                result.push_back(-1);
-            }else{
-                result.push_back(permuations[s2.substr(0,i)]);
-            }
-        }
-
         return result;
     }
 
 
+     /*
+
+        */
