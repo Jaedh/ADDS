ea20714a83c2933c9f2ec5c8d9f66b7ef940d605
Jaedh <jaedh.rameezdeen.jr@gmail.com>
2023-04-02 16:57:47 +0930

    Find

--- Finder.cpp
+++ Finder.cpp
@@ -4,11 +4,11 @@
 
     vector<int> Finder::findSubstrings(string s1, string s2) {
         vector<int> result;
-        size_t temp;
+        size_t temp = 0;
 
         for(size_t i = 1; i <= s2.size(); i++){
             if(temp != -1){             
-                temp = s1.find(s2.substr(0,i), i-1);
+                temp = s1.find(s2.substr(0,i), temp);
             }               
             result.push_back(temp);
         }
