40d1d5e4f661931af581f79adc3ee80453e9c3c6
Jaedh <jaedh.rameezdeen.jr@gmail.com>
2023-05-21 02:05:41 +0930

    Heap Initial

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include "Heap.tpp"
+#include <vector>
 
 int main(void){
+
+    std::vector<int> values = {1,2,3,4,5};
+    Heap<int> * h = new Heap<int>(values);
+    h->insert(10);
 
 }
